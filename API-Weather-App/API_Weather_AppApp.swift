26a930c0af1ba80a2e4451712d262739df66b385
Hilal <hilalparlakci>
2023-09-13 15:18:07 +0300

    Initial Commit

---
+++ API-Weather-App/API_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_Weather_AppApp.swift
+//  API-Weather-App
+//
+//  Created by Hilal on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct API_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
